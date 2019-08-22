b3ab41f57ecb0e9127233bea6399bc7b639e482e
jats2k9 <jats2k9@gmail.com>
2019-08-22 18:03:10 -0400

    Layout for creating user

---
+++ app/src/main/java/com/users/CreateUserActivity.java
@@ -0,0 +1,16 @@
+package com.users;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createuser);
+
+
+    }
+}
